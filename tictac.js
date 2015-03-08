c211cdf54504bb7dc50b06cd21647b93f06d5fc8
Melissa Skevington <mskeving@gmail.com>
2015-03-08 12:34:40 -0700

    code refactoring

--- tictac.js
+++ tictac.js
@@ -1,149 +1,198 @@
 (function($) {
-    var currentPlayer = 1; // 2 is computer
-    var gameBoard = [0,0,0,0,0,0,0,0,0];
-    var winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]];
+    var HUMAN = 1;
+    var COMPUTER = 2;
+    var WINNINGCOMBOS = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [6,4,2]
+    ];
 
     $(document).on('ready', function() {
-        $('.move').on('click', function() {
-            takeTurn($(this));
-        });
+        var gameState = initializeGameState();
+        humanTurn(gameState);
 
-        $('#new-game').on('click', function() {
-            resetBoard();
+        $('#reset-game').on('click', function() {
+            gameState = initializeGameState();
+            resetGameBoard();
+            humanTurn(gameState);
         });
     });
 
-    function resetBoard() {
-        gameBoard = [0,0,0,0,0,0,0,0,0];
-        $('.move').removeClass('winning-square player-1 player-2');
-        currentPlayer = 1;
+    function humanTurn(gameState) {
         changeStatus('Your move.');
-        $('.move').off('click'); // prevent multiple click handlers if resetting game before over
         $('.move').on('click', function() {
-            takeTurn($(this));
+            var squareID = this.id;
+
+            if (moveIsAvailable(gameState.board, squareID)) {
+                takeSquare(squareID, HUMAN);
+                gameState.board = updateBoard(gameState.board, squareID, HUMAN);
+                if (gameIsOver(gameState)) {
+                    removeClickHandler();
+                    displayResults(gameState);
+                } else {
+                    computerTurn(gameState);
+                }
+            } else {
+                changeStatus('Move is Taken');
+            }
         });
     }
 
-    function takeTurn(moveDivArray) {
-        changeStatus('');
-        // on click, check if spot is empty. If yes, color spot accordingly
-        if (moveIsTaken(moveDivArray[0])) {
-            changeStatus('Move is already taken');
-            return;
+    function computerTurn(gameState) {
+        var squareID;
+
+        removeClickHandler();
+        changeStatus('Computer\'s move.');
+        squareID = getComputerMove(gameState);
+
+        if (squareID === null) {
+            changeStatus('Error: can\'t find next move');
+        }
+
+        takeSquare(squareID, COMPUTER);
+        gameState.board = updateBoard(gameState.board, squareID, COMPUTER);
+
+        if (gameIsOver(gameState)){
+            displayResults(gameState);
         } else {
-            gameBoard[moveDivArray[0].id] = currentPlayer;
-            moveDivArray.addClass('player-' + currentPlayer);
-        }
-
-        // check if game is over with a win or tie
-        if (gameIsOver()) {
-            $('.move').off('click');
-            return;
-        }
-
-        // if not, change currentPlayer
-        currentPlayer = currentPlayer === 1 ? 2 : 1;
-        if (currentPlayer === 2) {
-            computerTurn();
+            humanTurn(gameState);
         }
     }
 
-    function computerTurn() {
-        var playerHas, playerDoesntHave;
+    function getComputerMove(gameState) {
+        // Computer tries to find winning move.
+        // If it can't, it tries to prevent human from winning.
+        // If neither of those are possible, take middle or next available square.
+        var nextMove;
 
-        if (winOrPreventWin()) { 
-            return;
-        }
+        function canWinOrPreventWin(potentialWinner) {
+            var combo, playerHas, playerDoesntHave;
+            for (combo=0; combo<WINNINGCOMBOS.length; combo++){
+                playerHas = [];
+                playerDoesntHave = [];
 
-        function winOrPreventWin() {
-            // Computer tries to find winning move.
-            // If it can't, it tries to prevent human from winning
+                playerAtFirst = gameState.board[WINNINGCOMBOS[combo][0]];
+                playerAtSecond = gameState.board[WINNINGCOMBOS[combo][1]];
+                playerAtThird = gameState.board[WINNINGCOMBOS[combo][2]];
 
-            // for each winning combination of moves, first check if player 2 (computer) can win in one move. If yes, take it.
-            // if not, check if player 1 (human) can win in one move. If yes, take it.
-            // Winning in one move means you have 2 out of 3 already in place.
-            for (var player=2; player>0; player--) {
-                for (var combo=0; combo<winningCombos.length; combo++){
-                    playerHas = [];
-                    playerDoesntHave = [];
-                    for (var idx=0; idx<winningCombos[combo].length; idx++) {
-                        gameBoardValue = gameBoard[winningCombos[combo][idx]];
-                        if (gameBoardValue === player) {
-                            playerHas.push(winningCombos[combo][idx]);
-                        } else {
-                            playerDoesntHave.push(winningCombos[combo][idx]);
-                        }
-                    }
-                    if (playerHas.length == 2 && !moveIsTaken($('#' + playerDoesntHave[0])[0])) {
-                        takeTurn($('#' + playerDoesntHave[0]));
-                        return true;
-                    }
+                if (playerAtFirst === potentialWinner) {
+                    playerHas.push(WINNINGCOMBOS[combo][0]);
+                } else {
+                    playerDoesntHave.push(WINNINGCOMBOS[combo][0]);
                 }
-            }
-            return false;
-        }
-        
-        // if computer can't winOrPreventWin, get middle square first (it's the best!), else just take next available square
-        if (moveIsTaken($('#4')[0])) {
-            for (var x=0; x<gameBoard.length; x++){
-                if (gameBoard[x] === 0 && !moveIsTaken($('#' + x)[0])) {
-                    takeTurn($('#' + x));
-                    return;
+
+                if (playerAtSecond === potentialWinner) {
+                    playerHas.push(WINNINGCOMBOS[combo][1]);
+                } else {
+                    playerDoesntHave.push(WINNINGCOMBOS[combo][1]);
                 }
-            }
-        } else {
-            takeTurn($('#4'));
-        }
-    }
 
-    function moveIsTaken(move) {
-        return gameBoard[move.id] !== 0;
-    }
-
-    function gameIsOver() {
-        var noMovesLeft = function() {
-            for (var i=0; i<gameBoard.length; i++){
-                if (gameBoard[i] === 0) {
-                    return false;
+                if (playerAtThird === potentialWinner) {
+                    playerHas.push(WINNINGCOMBOS[combo][2]);
+                } else {
+                    playerDoesntHave.push(WINNINGCOMBOS[combo][2]);
                 }
-            }
-            return true;
-        };
 
-        var isWin = function() {
-            var first, second, third;
-            var combos = winningCombos;
-
-            for (var i=0; i<winningCombos.length; i++){
-                idxArray = [combos[i][0], combos[i][1], combos[i][2]]
-                first = gameBoard[combos[i][0]],
-                second = gameBoard[combos[i][1]],
-                third = gameBoard[combos[i][2]];
-
-                // Win determined if gameboard has same value in all winningCombos indices (and doesn't equal 0).
-                if (first === second && second === third &&
-                        [first, second, third].indexOf(0) === -1) {
-                    for (var idx=0; idx<idxArray.length; idx++) {
-                        $('#' + idxArray[idx]).addClass('winning-square');
-                    }
+                if (playerHas.length === 2 && moveIsAvailable(gameState.board, playerDoesntHave[0])) {
+                    nextMove = playerDoesntHave[0];
                     return true;
                 }
             }
             return false;
-        };
-
-        if (isWin()){
-            (currentPlayer === 1) ? changeStatus('You win!') : changeStatus('You lost!');
-            return true;
-        } else if (noMovesLeft()) {
-            changeStatus("It's a tie!");
-            return true;
         }
-        return false;
+
+        if (canWinOrPreventWin(COMPUTER) || canWinOrPreventWin(HUMAN)) {
+            return nextMove;
+        } else if (moveIsAvailable(gameState.board, 4)) {
+            return 4;
+        } else {
+            for (var x=0; x<gameState.board.length; x++){
+                if (moveIsAvailable(gameState.board, gameState.board[x])) {
+                    return x;
+                }
+            }
+        }
+        return null;
+    }
+
+    function gameIsOver(gameState) {
+        gameState = checkForNewState(gameState);
+        return gameState.isWin || gameState.isDraw;
+    }
+
+    function checkForNewState(gameState) {
+        var playerAtFirst, playerAtSecond, playerAtThird;
+
+        if (gameState.board.indexOf(0) === -1) {
+            gameState.isDraw = true;
+            return gameState;
+        } else {
+            for (var combo=0; combo<WINNINGCOMBOS.length; combo++) {
+                playerAtFirst = gameState.board[WINNINGCOMBOS[combo][0]];
+                playerAtSecond = gameState.board[WINNINGCOMBOS[combo][1]];
+                playerAtThird = gameState.board[WINNINGCOMBOS[combo][2]];
+
+                if (playerAtFirst === playerAtSecond && playerAtSecond === playerAtThird &&
+                        [playerAtFirst, playerAtSecond, playerAtThird].indexOf(0) === -1) {
+                    gameState.isWin = true;
+                    gameState.winningCombo = WINNINGCOMBOS[combo];
+                    gameState.winner = playerAtFirst;
+                    return gameState;
+                }
+            }
+        }
+        return gameState;
+    }
+
+    function displayResults(gameState) {
+        if (gameState.isWin) {
+            for (var i=0; i<gameState.winningCombo.length; i++) {
+                $('#' + gameState.winningCombo[i]).addClass('winning-square');
+            }
+            (gameState.winner === HUMAN) ? changeStatus('You win!') : changeStatus('You lost!');
+        } else if (gameState.isDraw) {
+            changeStatus('It\'s a tie!');
+        }
+    }
+
+    function moveIsAvailable(gameBoard, id) {
+        return gameBoard[id] === 0;
+    }
+
+    function takeSquare(id, player) {
+        $('#' + id).addClass('player-' + player);
+    }
+
+    function updateBoard(gameBoard, id, player) {
+       gameBoard[id] = player;
+       return gameBoard;
+    }
+
+    function initializeGameState() {
+        return {
+            board: [0,0,0,0,0,0,0,0,0],
+            isDraw: false,
+            isWin: false,
+            winner: null,
+            winningombo: null
+        };
+    }
+
+    function resetGameBoard(){
+        $('.move').removeClass('winning-square player-1 player-2');
+        $('.move').off('click');
+    }
+
+    function removeClickHandler(){
+        $('.move').off('click');
     }
 
     function changeStatus(msg) {
         $('#status').text(msg);
     }
-
 })(jQuery);
